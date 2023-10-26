49174f673f8ac6ab22037295371097f66684e420
Adrynb <adriannavarrobuceta@gmail.com>
2023-10-26 20:38:57 +0200

    Initial commit

---
+++ app/src/main/java/com/example/practica1_pmdm/ANB_primosXActivity.kt
@@ -0,0 +1,41 @@
+package com.example.practica1_pmdm
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ANB_primosXActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_anb_xactivity)
+        var numero = intent.getIntExtra("numero", -1)
+        var resultadoPrimo = cal_primos(numero)
+
+        val returnIntent = Intent()
+        returnIntent.putExtra("Resultado", resultadoPrimo.toString())
+        setResult(RESULT_OK, returnIntent)
+        finish()
+    }
+
+    fun cal_primos(n: Int): ArrayList<Int> {
+        val elementos = ArrayList<Int>()
+
+        for (i in 1..n) {
+            if (esPrimo(i, i - 1)) {
+                elementos.add(i)
+            }
+        }
+        return elementos
+    }
+
+    private fun esPrimo(n: Int, divisor: Int): Boolean {
+        if (divisor <= 1) {
+            return true
+        } else if (n % divisor == 0) {
+            return false
+        } else {
+            return esPrimo(n, divisor - 1)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/practica1_pmdm/MainActivity.kt
@@ -0,0 +1,88 @@
+package com.example.practica1_pmdm
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    var EDR: Int = 0
+    var contador: Int = 0
+    lateinit var buttonReturn: Button
+    lateinit var buttonIraActivityPrimos: Button
+    lateinit var buttonReturnX: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        EDR = 0
+        contador++
+        Log.i("ESTADOS", "Creando actividad:" + contador)
+        Log.i("ESTADOS", "Evento onCreate:" + contador)
+
+        buttonReturn = findViewById(R.id.buttonReturn)
+        buttonReturn.setOnClickListener {
+            val intent = Intent(this, ANB_numPrimos::class.java)
+            intent.putExtra("numero", 15)
+            startActivityForResult(intent, 1)
+        }
+
+        buttonReturnX = findViewById(R.id.buttonReturnX)
+
+        buttonReturnX.setOnClickListener {
+            val intent = Intent(this, ANB_primosXActivity::class.java)
+            intent.putExtra("numero", 10)
+            edrPrimosResultLauncher.launch(intent)
+        }
+    }
+
+    private val edrPrimosResultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val data: Intent? = result.data
+            val resultado = data?.getStringExtra("Resultado")
+            Log.i("Resultado", resultado!!)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            val resultado = data?.getStringExtra("Resultado")
+            Log.i("Resultado", resultado!!)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        contador++
+        EDR++
+        Log.i("ESTADOS", "Evento onStart:" + contador)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        contador++
+        EDR++
+        Log.i("ESTADOS", "Evento onResume:" + contador)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        contador++
+        EDR++
+        Log.i("ESTADOS", "Evento onStop:" + contador)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        EDR++
+        Log.i("ESTADOS", "Evento onDestroy:" + contador)
+    }
+}
