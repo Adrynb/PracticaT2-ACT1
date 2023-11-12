21ccdd09b332636f29c53354b21d3429248eda7c
BuildTools <unconfigured@null.spigotmc.org>
2023-11-12 14:25:46 +0100

    BroadCaster (Completo)

--- app/src/main/java/com/example/practica1_pmdm/BroadCastReciver/MyReciver.kt
+++ app/src/main/java/com/example/practica1_pmdm/BroadCastReciver/MyReciver.kt
@@ -9,23 +9,22 @@ import android.widget.Toast
 class MyReceiver(private val textView: TextView) : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
-        val mensaje: String = when (intent?.action) {
-            Intent.ACTION_BATTERY_LOW -> {
-                "Estado actual: Batería Baja"
+
+        val action = intent?.action
+
+        if (action == Intent.ACTION_BATTERY_LOW) {
+            val mensaje = "Estado actual: Batería Baja"
+            Toast.makeText(context, mensaje, Toast.LENGTH_LONG).show()
+            textView.text = mensaje
+        } else if (action == Intent.ACTION_AIRPLANE_MODE_CHANGED) {
+            val isAirplaneModeOn = intent.getBooleanExtra("state", false)
+            val mensaje = if (isAirplaneModeOn) {
+                "Estado actual: Modo Avión Activado"
+            } else {
+                "Estado actual: Modo Avión Desactivado"
             }
-            Intent.ACTION_AIRPLANE_MODE_CHANGED -> {
-                val isAirplaneModeOn = intent.getBooleanExtra("state", false)
-                if (isAirplaneModeOn) {
-                    "Estado actual: Modo Avión Activado"
-                } else {
-                    "Estado actual: Modo Avión Desactivado"
-                }
-            }
-            else -> return
+            Toast.makeText(context, mensaje, Toast.LENGTH_LONG).show()
+            textView.text = mensaje
         }
-
-        Toast.makeText(context, mensaje, Toast.LENGTH_LONG).show()
-
-        textView.text = mensaje
     }
 }
